4985d31c666bfe7c2a42091c9f293b8eef5b2eee
Jeong, Kyung Hoon <khjung@youngincm.com>
2022-09-21 16:30:38 +0900

    ChromassProtocal.APIs

---
+++ ChromassProtocol/APIs/Base.cs
@@ -0,0 +1,28 @@
+﻿using ChromassProtocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromassProtocols.APIs
+{
+    public class Base<TPacket> : UnionActionsAfter100ms where TPacket : struct
+    {
+        public PacketWrapper<TPacket> Wrapper { get; init; }
+
+        public Base(PacketWrapper<TPacket> wrapper, Action? action) : base(action)
+        {
+            Wrapper = wrapper;
+        }
+
+        public virtual void Assign(Base<TPacket> src)
+        {
+            if (src != this && src.Wrapper != Wrapper)
+            {
+                src.Wrapper.Packet = src.Wrapper.Packet;
+                CallAction();
+            }
+        }
+    }
+}
---
+++ ChromassProtocol/APIs/UnionActionsAfter100ms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromassProtocols.APIs
+{
+    public class UnionActionsAfter100ms
+    {
+        private Action? action;
+        private int mergeCounts; 
+
+        public UnionActionsAfter100ms(Action? action)
+        {
+            this.action = action;
+        }
+
+        public bool IsActive => action != null;
+
+        public async void CallAction()
+        {
+            mergeCounts++;
+            await Task.Delay(100);
+            if(--mergeCounts == 0)
+            {
+                action?.Invoke();
+            }
+        }
+    }
+}
