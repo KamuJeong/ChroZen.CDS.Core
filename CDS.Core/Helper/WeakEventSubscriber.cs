f281aeeaa6dd6e742dbd42a2140d496ad77a63b2
Jeong, Kyung Hoon <khjung@youngincm.com>
2022-10-21 16:45:27 +0900

    add WeakEventSubscriber

---
+++ CDS.Core/Helper/WeakEventSubscriber.cs
@@ -0,0 +1,35 @@
+﻿namespace CDS.Core
+{
+    public abstract class WeakEventSubscriber<TSub, TPub> where TSub : class
+                                                            where TPub : class
+    {
+        private WeakReference<TSub> Subscriber
+        {
+            get; init;
+        }
+        protected TPub Publisher
+        {
+            get; init;
+        }
+
+        public WeakEventSubscriber(TSub subscriber, TPub publisher)
+        {
+            Subscriber = new WeakReference<TSub>(subscriber, false);
+            Publisher = publisher;
+        }
+
+        public abstract void SubScribe();
+        public abstract void Unsubscribe();
+
+        protected TSub? GetSubscriber()
+        {
+            if (Subscriber.TryGetTarget(out TSub? subscriber))
+            {
+                return subscriber;
+            }
+
+            Unsubscribe();
+            return null;
+        }
+    }
+}
