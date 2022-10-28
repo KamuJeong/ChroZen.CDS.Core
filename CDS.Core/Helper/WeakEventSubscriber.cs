38055d02a8f6cdaeffa01f17057b210c1d57cb6e
Jeong, Kyung Hoon <khjung@youngincm.com>
2022-10-28 18:04:33 +0900

    recreate WeakEventSubscriber

--- CDS.Core/Helper/WeakEventSubscriber.cs
+++ CDS.Core/Helper/WeakEventSubscriber.cs
@@ -1,35 +1,51 @@
 ﻿namespace CDS.Core
 {
-    public abstract class WeakEventSubscriber<TSub, TPub> where TSub : class
-                                                            where TPub : class
+    public sealed class WeakEventSubscriber<TSub, TArgs> where TSub : class
+                                                    where TArgs : EventArgs
     {
-        private WeakReference<TSub> Subscriber
+        private readonly WeakReference<TSub> subscriber;
+        private readonly EventHandler<TArgs> handler;
+        private readonly Action<EventHandler<TArgs>> unsubscribe;
+        private readonly SynchronizationContext? synchronizationContext;
+
+        public WeakEventSubscriber(TSub subscriber,
+                                        EventHandler<TArgs> handler,
+                                        Action<EventHandler<TArgs>> subscribe,
+                                        Action<EventHandler<TArgs>> unsubscribe)
         {
-            get; init;
-        }
-        protected TPub Publisher
-        {
-            get; init;
+            this.subscriber = new WeakReference<TSub>(subscriber, false);
+            this.handler = handler;
+            this.unsubscribe = unsubscribe;
+            synchronizationContext = SynchronizationContext.Current;
+
+            subscribe(Handler);
         }
 
-        public WeakEventSubscriber(TSub subscriber, TPub publisher)
+        private void Handler(object? sender, TArgs args)
         {
-            Subscriber = new WeakReference<TSub>(subscriber, false);
-            Publisher = publisher;
-        }
-
-        public abstract void SubScribe();
-        public abstract void Unsubscribe();
-
-        protected TSub? GetSubscriber()
-        {
-            if (Subscriber.TryGetTarget(out TSub? subscriber))
+            void PostHandler(object? e)
             {
-                return subscriber;
+                if (e is Tuple<object?, TArgs> v)
+                {
+                    handler(v.Item1, v.Item2);
+                }
             }
 
-            Unsubscribe();
-            return null;
+            if (subscriber.TryGetTarget(out TSub? sub))
+            {
+                if (synchronizationContext != null && synchronizationContext != SynchronizationContext.Current)
+                {
+                    synchronizationContext.Post(new SendOrPostCallback(PostHandler), new Tuple<object?, TArgs>(sender, args));
+                }
+                else
+                {
+                    handler(sender, args);
+                }
+            }
+            else
+            {
+                unsubscribe(Handler);
+            }
         }
     }
 }
